af0e868b5b0cb4e34d55b7127c3b5e4f8370b582
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-04-16 15:45:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,34 @@
-echo "python /data/data/com.termux/files/usr/bin/Tmig/run.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "#========================================||"
+echo "#================PROJECT INFO============||"
+echo "#========================================||"
+echo "#==== Auther : SM02 PresenT =============||"
+echo "#==== Start Date : 07/04/2022 ===========||"
+echo "#==== End Date : 11/04/2022 =============||"
+echo "#==== Version : 1.0 BETA ================||"
+echo "#==== About : Termx Photo ViewR =========||"
+echo "#==== Note : Do Not Copy My Code , ======||"
+echo "#==== Otherwise Deleting My project =====||"
+echo "#========================================||"
+echo "#================END INFO================||"
+echo "#========================================||"
+apt update
+apt install python git wget curl -y
+clear
+echo "#========================================||"
+echo "#================PROJECT INFO============||"
+echo "#========================================||"
+echo "#==== Auther : SM02 PresenT =============||"
+echo "#==== Start Date : 07/04/2022 ===========||"
+echo "#==== End Date : 11/04/2022 =============||"
+echo "#==== Version : 1.0 BETA ================||"
+echo "#==== About : Termx Photo ViewR =========||"
+echo "#==== Note : Do Not Copy My Code , ======||"
+echo "#==== Otherwise Deleting My project =====||"
+echo "#========================================||"
+echo "#================END INFO================||"
+echo "#========================================||"
+
+cd /data/data/com.termux/files/usr/bin/
+git clone https://github.com/Simplehacker1Community/Timg
+cd Timg
+python main.py
